8d3cea7249f35fb1132ffb4fad65e9c282e3927c
dante1995 <behera.sabyasachi2@gmail.com>
2015-04-08 20:24:23 +0530

    new windows added

--- UI/user/show_flights.py
+++ UI/user/show_flights.py
@@ -7,24 +7,45 @@ import MySQLdb
 import time
 import sys
 import datetime
-from book_window import *
 from passenger_entry import *
 class show_flights(QDialog):
-    def __init__(self):
+    def __init__(self,no,dest,source,date):
         super(show_flights,self).__init__()
+        self.no = str(no)
+        self.dest = str(dest)
+        self.source = str(source)
+        self.date = str(date)
+        # print "no in show flights = " + self.no
         self.initUI()
 
     def initUI(self):
+        while 1:
+            try:
+                self.db = MySQLdb.connect("10.5.18.66","12CS10041","btech12","12CS10041")
+                break
+            except:
+                time.sleep(.1)
+                continue
 
-        # while 1:
-        # try:
-        #         self.db = MySQLdb.connect("10.5.18.66","12CS10042","btech12","12CS10042")
-        #         break
-        #     except:
-        #         time.sleep(.1)
-        #         continue
 
-        # self.cursor = self.db.cursor()
+        self.cursor = self.db.cursor()
+
+        sql = "select airport_id from airport where name like \"%"+self.source+"%\";"
+        # print sql
+        self.cursor.execute(sql)
+        sid = self.cursor.fetchall()
+
+        sql = "select airport_id from airport where name like \"%"+self.dest+"%\";"
+        self.cursor.execute(sql)
+        did = self.cursor.fetchall()
+        print did[0][0]
+        # print (did[0][0]).split('\'')
+        sql = "select flight_id, available, arrival, departure from schedule,route,airport as A,airport as B where flight_date = \""+ self.date + "\" and available >= "+ str(self.no) +" and A.city = \"" + str(self.source) +"\" and B.city = \""+str(self.dest)+ "\" and route.source = A.airport_id and route.destination = B.airport_id and schedule.route_id = route.route_id;"
+        print sql
+        self.cursor.execute(sql)
+        results = self.cursor.fetchall()
+
+
         flights = ['dasd','fdgvdf ','dfewrf']
         source = ['dsv','fv','fv']
         dest = ['vcfsd','vd','dsvc']
@@ -44,18 +65,19 @@ class show_flights(QDialog):
         # self.setLayout(self.grid)
         self.table = QTableWidget()
         self.table.setRowCount(size)
-        self.table.setColumnCount(5)
-        self.table.setHorizontalHeaderLabels(QString("Flight ID;From;To ;Availability;Time").split(";"));
+        self.table.setColumnCount(6)
+        self.table.setHorizontalHeaderLabels(QString("Flight ID;From;To ;Availability;Arrival;Departure").split(";"));
 
         self.table.horizontalHeader().setResizeMode(QtGui.QHeaderView.Stretch)
         self.table.resizeColumnsToContents()
 
         for i in range(size):
-            self.table.setItem(i,0,QTableWidgetItem(flights[i]))
-            self.table.setItem(i,1,QTableWidgetItem(source[i]))
-            self.table.setItem(i,2,QTableWidgetItem(dest[i]))
-            self.table.setItem(i,3,QTableWidgetItem("100"))
-            self.table.setItem(i,4,QTableWidgetItem("10:00"))
+            self.table.setItem(i,0,QTableWidgetItem(results[i][0]))
+            self.table.setItem(i,1,QTableWidgetItem(self.source))
+            self.table.setItem(i,2,QTableWidgetItem(self.dest))
+            self.table.setItem(i,3,QTableWidgetItem(results[i][1]))
+            self.table.setItem(i,4,QTableWidgetItem(results[i][2]))
+            self.table.setItem(i,4,QTableWidgetItem(results[i][3]))
 
 
         self.grid.addWidget(self.table,0,0,1,-1)
@@ -66,10 +88,18 @@ class show_flights(QDialog):
 
     def ok_func(self):
         self.id =  self.table.currentItem().text()
-        pass_ent = passenger_entry(self.id)
+        pass_ent = passenger_entry(self.id, self.no)
         pass_ent.exec_()
         print "ticket"
     def cancel_func(self):
         self.close()
-        x=3
 
+# def main():
+#     app = QApplication(sys.argv)
+#     start = show_flights()
+#     start.show()
+#     app.exec_()
+#
+#
+# if __name__ == '__main__':
+#     main()
